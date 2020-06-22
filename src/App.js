e888e657007271bf4d1fb127e61dce3dadf9ca27
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-06-22 15:17:25 -0400

    added the counter class & the count on state & added CSS and JSX

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  };
+
+  render() {
+    return (
+      // parent div block for 
+      // 1. Counter.js nav bar
+      // 2. counter # display using JS code: {this.state.count}
+      // 3. buttons (increment, decrement)
+      <div className = "container">
+        <div className="navbar">Counter.js</div>
+        <div className="counter">
+          <h1>{this.state.count}</h1>
+          <button type="button">Increment</button>
+          <button type="button">Decrement</button>
+        </div>
+      </div>
+      );
+  };
+};
+
+export default Counter;
+
+/* 
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -24,3 +55,18 @@ function App() {
 }
 
 export default App;
+*/
+
+/*
+import React, {Component} from 'react';
+
+class HelloWorld extends Component {
+  render() {
+    return(
+      <div>Hello World</div>
+    );
+  };
+};
+
+export default HelloWorld;
+*/
